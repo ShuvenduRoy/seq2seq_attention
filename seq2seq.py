620cee2fb4480dc2fe462016cd63b90b083e41e8
Shuvendu Bikash <bikash11roy@gmail.com>
2018-03-27 08:51:25 +0600

    Test code bug fixed

--- seq2seq.py
+++ seq2seq.py
@@ -102,13 +102,16 @@ if __name__ == '__main__':
     outputs = list(Yoh.swapaxes(0, 1))
 
     model.fit([Xoh, s0, c0], outputs, epochs=10, batch_size=100)
+    model.load_weights('model.h5')
 
     """ Test the model """
+
     EXAMPLES = ['3 May 1979', '5 April 09', '21th of August 2016', 'Tue 10 Jul 2007', 'Saturday May 9 2018',
                 'March 3 2001', 'March 3rd 2001', '1 March 2001']
     for example in EXAMPLES:
         source = string_to_int(example, Tx, human_vocab)
-        source = np.array(list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), source))).swapaxes(0, 1)
+        source = np.array(
+            list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), source)))  # .swapaxes(0,1)
         prediction = model.predict([source.reshape(1, 30, 37), s0, c0])
         prediction = np.argmax(prediction, axis=-1)
         output = [inv_machine_vocab[int(i)] for i in prediction]
